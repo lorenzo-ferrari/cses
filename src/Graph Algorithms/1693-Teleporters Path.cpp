918b2824d329361a6f5c1f30314d4bf90001ee4f
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2025-12-09 19:23:49 +0100

    .

--- src/Graph Algorithms/1693-Teleporters Path.cpp
+++ src/Graph Algorithms/1693-Teleporters Path.cpp
@@ -1,53 +1,47 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
- 
+using LL = long long;
+
 int main() {
-    int n, m;
-    cin >> n >> m;
+	int n; cin >> n;
+	int m; cin >> m;
+	vector<int> deg(n + 1);
+	vector<multiset<int>> adj(n + 1);
+	for (int i = 0, a, b; i < m; ++i) {
+		cin >> a >> b;
+		adj[a].insert(b);
+		++deg[a];
+		--deg[b];
+	}
+	--deg[1];
+	++deg[n];
+	adj[n].insert(1);
+	if (*max_element(begin(deg), end(deg)) != 0) {
+		cout << "IMPOSSIBLE\n";
+		return 0;
+	}
+	vector<int> tour;
+	auto dfs = [&](auto &&self, int v) -> void {
+		while (!adj[v].empty()) {
+			int u = *begin(adj[v]);
+			adj[v].erase(begin(adj[v]));
+			self(self, u);
+		}
+		tour.push_back(v);
+	};
 
-    vector<int> d(n + 1);
-    vector<set<int>> g(n + 1);
-    for (int i = 0, a, b; i < m; i++) {
-        cin >> a >> b;
-        g[a].insert(b);
-        ++d[a];
-        --d[b];
-    }
- 
-    function<void()> ret = [&]() {
-        cout << "IMPOSSIBLE" << "\n";
-        exit(0);
-    };
-
-    if (d[1] ^ 1 || d[n] ^ -1)
-        ret();
-    for (int i = 2; i < n; ++i)
-        if (d[i])
-            ret();
-
-    vector<int> ans;
-    function<void(int)> find_euler_cycle = [&](int v) {
-        while (!g[v].empty()) {
-            int u = *g[v].begin();
-            g[v].erase(u);
-            find_euler_cycle(u);
-        }
-        ans.push_back(v);
-    };
- 
-    g[n].insert(1);
-    find_euler_cycle(1);
-    reverse(ans.begin(), ans.end());
-    ans.pop_back();
-
-    if (ans.size() ^ (m + 1)) ret();
-
-    int s;
-    for (s = 0; s <= m; ++s)
-        if (ans[s] == 1 && ans[(s + m) % (m + 1)] == n)
-            break;
-
-    for (int i = s; i <= m; ++i) cout << ans[i] << " ";
-    for (int i = 0; i <  s; ++i) cout << ans[i] << " ";
-    cout << "\n";
+	dfs(dfs, 1);
+	tour.pop_back();
+	reverse(begin(tour), end(tour));
+	if ((int)tour.size() != m + 1) {
+		cout << "IMPOSSIBLE\n";
+	} else {
+		int i = 0;
+		while (tour[i] != n || tour[(i + 1) % (m + 1)] != 1)
+			++i;
+		rotate(begin(tour), begin(tour) + i + 1, end(tour));
+		for (int x : tour)
+			cout << x << " ";
+		cout << "\n";
+	}
 }
